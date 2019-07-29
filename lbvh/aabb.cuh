4534c008f320ed5318b20e27a6254068c7145657
ToruNiina <niina.toru.68u@gmail.com>
2019-07-30 00:28:00 +0900

    :sparkles: add mindist/minmaxdist for nearest neighbor

--- lbvh/aabb.cuh
+++ lbvh/aabb.cuh
@@ -1,6 +1,7 @@
 #ifndef LBVH_AABB_CUH
 #define LBVH_AABB_CUH
 #include "utility.cuh"
+#include <thrust/swap.h>
 #include <cmath>
 
 namespace lbvh
@@ -47,6 +48,81 @@ inline aabb<float> merge(const aabb<float>& lhs, const aabb<float>& rhs) noexcep
     merged.lower.y = ::fminf(lhs.lower.y, rhs.lower.y);
     merged.lower.z = ::fminf(lhs.lower.z, rhs.lower.z);
     return merged;
+}
+
+// metrics defined in
+// Nearest Neighbor Queries (1995) ACS-SIGMOD
+// - Nick Roussopoulos, Stephen Kelley FredericVincent
+
+__device__ __host__
+inline float mindist(const aabb<float>& lhs, const float4& rhs) noexcept
+{
+    const float dx = ::fminf(lhs.upper.x, ::fmaxf(lhs.lower.x, rhs.x)) - rhs.x;
+    const float dy = ::fminf(lhs.upper.y, ::fmaxf(lhs.lower.y, rhs.y)) - rhs.y;
+    const float dz = ::fminf(lhs.upper.z, ::fmaxf(lhs.lower.z, rhs.z)) - rhs.z;
+    return dx * dx + dy * dy + dz * dz;
+}
+__device__ __host__
+inline double mindist(const aabb<double>& lhs, const double4& rhs) noexcept
+{
+    const double dx = ::fmin(lhs.upper.x, ::fmax(lhs.lower.x, rhs.x)) - rhs.x;
+    const double dy = ::fmin(lhs.upper.y, ::fmax(lhs.lower.y, rhs.y)) - rhs.y;
+    const double dz = ::fmin(lhs.upper.z, ::fmax(lhs.lower.z, rhs.z)) - rhs.z;
+    return dx * dx + dy * dy + dz * dz;
+}
+
+__device__ __host__
+inline float minmaxdist(const aabb<float>& lhs, const float4& rhs) noexcept
+{
+    float3 rm_sq = make_float3((lhs.lower.x - rhs.x) * (lhs.lower.x - rhs.x),
+                               (lhs.lower.y - rhs.y) * (lhs.lower.y - rhs.y),
+                               (lhs.lower.z - rhs.z) * (lhs.lower.z - rhs.z));
+    float3 rM_sq = make_float3((lhs.upper.x - rhs.x) * (lhs.upper.x - rhs.x),
+                               (lhs.upper.y - rhs.y) * (lhs.upper.y - rhs.y),
+                               (lhs.upper.z - rhs.z) * (lhs.upper.z - rhs.z));
+    if((lhs.upper.x + lhs.lower.x) * 0.5 < rhs.x)
+    {
+        thrust::swap(rm_sq.x, rM_sq.x);
+    }
+    if((lhs.upper.y + lhs.lower.y) * 0.5 < rhs.y)
+    {
+        thrust::swap(rm_sq.y, rM_sq.y);
+    }
+    if((lhs.upper.z + lhs.lower.z) * 0.5 < rhs.z)
+    {
+        thrust::swap(rm_sq.z, rM_sq.z);
+    }
+    const float dx = rm_sq.x + rM_sq.y + rM_sq.z;
+    const float dy = rM_sq.x + rm_sq.y + rM_sq.z;
+    const float dz = rM_sq.x + rM_sq.y + rm_sq.z;
+    return ::fminf(dx, ::fminf(dy, dz));
+}
+
+__device__ __host__
+inline double minmaxdist(const aabb<double>& lhs, const double4& rhs) noexcept
+{
+    double3 rm_sq = make_double3((lhs.lower.x - rhs.x) * (lhs.lower.x - rhs.x),
+                                 (lhs.lower.y - rhs.y) * (lhs.lower.y - rhs.y),
+                                 (lhs.lower.z - rhs.z) * (lhs.lower.z - rhs.z));
+    double3 rM_sq = make_double3((lhs.upper.x - rhs.x) * (lhs.upper.x - rhs.x),
+                                 (lhs.upper.y - rhs.y) * (lhs.upper.y - rhs.y),
+                                 (lhs.upper.z - rhs.z) * (lhs.upper.z - rhs.z));
+    if((lhs.upper.x + lhs.lower.x) * 0.5 < rhs.x)
+    {
+        thrust::swap(rm_sq.x, rM_sq.x);
+    }
+    if((lhs.upper.y + lhs.lower.y) * 0.5 < rhs.y)
+    {
+        thrust::swap(rm_sq.y, rM_sq.y);
+    }
+    if((lhs.upper.z + lhs.lower.z) * 0.5 < rhs.z)
+    {
+        thrust::swap(rm_sq.z, rM_sq.z);
+    }
+    const double dx = rm_sq.x + rM_sq.y + rM_sq.z;
+    const double dy = rM_sq.x + rm_sq.y + rM_sq.z;
+    const double dz = rM_sq.x + rM_sq.y + rm_sq.z;
+    return ::fminf(dx, ::fminf(dy, dz));
 }
 
 } // lbvh
