967b0e4c6875619d857e33755869e1abb4613d0a
ToruNiina <niina.toru.68u@gmail.com>
2019-08-03 15:11:04 +0900

    :bug: fix query_host

--- lbvh/query.cuh
+++ lbvh/query.cuh
@@ -161,28 +161,34 @@ thrust::pair<unsigned int, Real> query_device(
 template<typename Real, typename Objects, typename AABBGetter,
          typename MortonCodeCalculator, typename OutputIterator>
 unsigned int query_host(
-    const bvh<Real, Objects, AABBGetter, MortonCodeCalculator>& bvh,
+    const bvh<Real, Objects, AABBGetter, MortonCodeCalculator>& tree,
     const query_overlap<Real> q, OutputIterator outiter,
-    const unsigned int max_buffer_size = 0xFFFFFFFF) noexcept
+    const unsigned int max_buffer_size = 0xFFFFFFFF)
 {
-    using bvh_type   = detail::basic_device_bvh<Real, Objects, IsConst>;
+    using bvh_type   = ::lbvh::bvh<Real, Objects, AABBGetter, MortonCodeCalculator>;
     using index_type = typename bvh_type::index_type;
     using aabb_type  = typename bvh_type::aabb_type;
     using node_type  = typename bvh_type::node_type;
 
-    std::vector<std::size_t> stack = {0};
+    if(!tree.query_host_enabled())
+    {
+        throw std::runtime_error("lbvh::bvh query_host is not enabled");
+    }
+
+    std::vector<std::size_t> stack;
     stack.reserve(64);
+    stack.push_back(0);
 
     unsigned int num_found = 0;
     do
     {
         const index_type node  = stack.back(); stack.pop_back();
-        const index_type L_idx = bvh.nodes_host()[node].left_idx;
-        const index_type R_idx = bvh.nodes_host()[node].right_idx;
+        const index_type L_idx = tree.nodes_host()[node].left_idx;
+        const index_type R_idx = tree.nodes_host()[node].right_idx;
 
-        if(intersects(q.target, bvh.aabbs_host()[L_idx]))
+        if(intersects(q.target, tree.aabbs_host()[L_idx]))
         {
-            const auto obj_idx = bvh.nodes_host()[L_idx].object_idx;
+            const auto obj_idx = tree.nodes_host()[L_idx].object_idx;
             if(obj_idx != 0xFFFFFFFF)
             {
                 if(num_found < max_buffer_size)
@@ -196,9 +202,9 @@ unsigned int query_host(
                 stack.push_back(L_idx);
             }
         }
-        if(intersects(q.target, bvh.aabbs_host()[R_idx]))
+        if(intersects(q.target, tree.aabbs_host()[R_idx]))
         {
-            const auto obj_idx = bvh.nodes_host()[R_idx].object_idx;
+            const auto obj_idx = tree.nodes_host()[R_idx].object_idx;
             if(obj_idx != 0xFFFFFFFF)
             {
                 if(num_found < max_buffer_size)
@@ -213,25 +219,30 @@ unsigned int query_host(
             }
         }
     }
-    while (stack < stack_ptr);
+    while (!stack.empty());
     return num_found;
 }
 
 template<typename Real, typename Objects, typename AABBGetter,
          typename MortonCodeCalculator, typename DistanceCalculator>
 std::pair<unsigned int, Real> query_host(
-        const bvh<Real, Objects, AABBGetter, MortonCodeCalculator>& bvh,
+        const bvh<Real, Objects, AABBGetter, MortonCodeCalculator>& tree,
         const query_nearest<Real>& q, DistanceCalculator calc_dist) noexcept
 {
-    using bvh_type   = detail::basic_device_bvh<Real, Objects, IsConst>;
+    using bvh_type   = ::lbvh::bvh<Real, Objects, AABBGetter, MortonCodeCalculator>;
     using real_type  = typename bvh_type::real_type;
     using index_type = typename bvh_type::index_type;
     using aabb_type  = typename bvh_type::aabb_type;
     using node_type  = typename bvh_type::node_type;
 
+    if(!tree.query_host_enabled())
+    {
+        throw std::runtime_error("lbvh::bvh query_host is not enabled");
+    }
+
     // pair of {node_idx, mindist}
     std::vector<std::pair<index_type, real_type>> stack = {
-        {0, mindist(bvh.aabbs_host()[0], q.target)}
+        {0, mindist(tree.aabbs_host()[0], q.target)}
     };
     stack.reserve(64);
 
@@ -246,11 +257,11 @@ std::pair<unsigned int, Real> query_host(
             continue;
         }
 
-        const index_type L_idx = bvh.nodes_host()[node.first].left_idx;
-        const index_type R_idx = bvh.nodes_host()[node.first].right_idx;
+        const index_type L_idx = tree.nodes_host()[node.first].left_idx;
+        const index_type R_idx = tree.nodes_host()[node.first].right_idx;
 
-        const aabb_type& L_box = bvh.aabbs_host()[L_idx];
-        const aabb_type& R_box = bvh.aabbs_host()[R_idx];
+        const aabb_type& L_box = tree.aabbs_host()[L_idx];
+        const aabb_type& R_box = tree.aabbs_host()[R_idx];
 
         const real_type L_mindist = mindist(L_box, q.target);
         const real_type R_mindist = mindist(R_box, q.target);
@@ -264,10 +275,10 @@ std::pair<unsigned int, Real> query_host(
 
         if(L_mindist <= R_minmaxdist) // L is worth considering
         {
-            const auto obj_idx = bvh.nodes_host()[L_idx].object_idx;
+            const auto obj_idx = tree.nodes_host()[L_idx].object_idx;
             if(obj_idx != 0xFFFFFFFF) // leaf node
             {
-                const real_type dist = calc_dist(q.target, bvh.objects_host()[obj_idx]);
+                const real_type dist = calc_dist(q.target, tree.objects_host()[obj_idx]);
                 if(dist <= current_nearest_dist)
                 {
                     current_nearest_dist = dist;
@@ -276,15 +287,15 @@ std::pair<unsigned int, Real> query_host(
             }
             else
             {
-                *stack_ptr++ = std::make_pair(L_idx, L_mindist);
+                stack.emplace_back(L_idx, L_mindist);
             }
         }
         if(R_mindist <= L_minmaxdist) // R is worth considering
         {
-            const auto obj_idx = bvh.nodes_host()[R_idx].object_idx;
+            const auto obj_idx = tree.nodes_host()[R_idx].object_idx;
             if(obj_idx != 0xFFFFFFFF) // leaf node
             {
-                const real_type dist = calc_dist(q.target, bvh.objects_host()[obj_idx]);
+                const real_type dist = calc_dist(q.target, tree.objects_host()[obj_idx]);
                 if(dist <= current_nearest_dist)
                 {
                     current_nearest_dist = dist;
@@ -293,11 +304,11 @@ std::pair<unsigned int, Real> query_host(
             }
             else
             {
-                *stack_ptr++ = std::make_pair(R_idx, R_mindist);
+                stack.emplace_back(R_idx, R_mindist);
             }
         }
     }
-    while (stack < stack_ptr);
+    while (!stack.empty());
     return std::make_pair(nearest, current_nearest_dist);
 }
 } // lbvh
